f124633896e3fa07b7228be1c139573660bee561
Danny Robinson <drobinson@healthgrades.com>
2016-07-22 16:33:54 -0600

    Trying to redo configuration. added gitignore

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using TodoApp2.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Abstractions;
 
 [assembly: OwinStartup(typeof(TodoApp2.Startup))]
 
@@ -10,9 +15,21 @@ namespace TodoApp2
 {
     public partial class Startup
     {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.AddLogging();
+
+            // Add our repository type
+            services.AddSingleton<ITodoRepository, TodoRepository>();
+        }
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
         }
+
     }
 }
